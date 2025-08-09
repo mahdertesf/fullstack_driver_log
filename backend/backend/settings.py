7f864af032ed6efe50c8ace1a43fa3c1a71bd56d
Mahder Tesfaye Abebe <mahdertesfaye11@gmail.com>
2025-08-09 03:41:32 +0300

    Fix formatting in middleware section of settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -34,6 +34,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'backend.urls'
